7b28c4b3a15fc27b2f71afc8c4d981f16687a074
jslvtr <jslvtr@gmail.com>
2015-08-09 22:14:02 +0100

    Moved the default build() method to the `InputField` type as opposed to it sitting in `BaseField` and `InputField` inheriting it, but having nothing else inside it.

--- src/form/fields.py
+++ src/form/fields.py
@@ -41,6 +41,11 @@ class BaseField(object):
         else:
             self.attrs[key] = value
 
+
+class InputField(BaseField):
+    def __init__(self, attrs={}, parent=None, children=[]):
+        super(InputField, self).__init__(attrs, parent, children)
+
     def build(self):
         html = "<{}".format(self.tag)
         for index, key in enumerate(self.attrs.keys()):
@@ -48,11 +53,6 @@ class BaseField(object):
             html += ' {}="{}"'.format(key, " ".join(value) if isinstance(value, list) else value)
         html += ">"
         return html
-
-
-class InputField(BaseField):
-    def __init__(self, attrs={}, parent=None, children=[]):
-        super(InputField, self).__init__(attrs, parent, children)
 
 
 class SelectField(BaseField):
